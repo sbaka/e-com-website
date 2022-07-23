b603df7dcd18d670d8f200dcb498f01c2574d98c
Maaaadox <ahmedmimen9@gmail.com>
2022-07-23 17:54:09 +0100

    sign up done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import Login from "./pages/Login_page";
 import Register from "./pages/Register_page";
-import Cringe from "./pages/Cringe";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Register />);
--- my-app/src/pages/Register_page.jsx
+++ my-app/src/pages/Register_page.jsx
@@ -1,3 +1,5 @@
+//TODO : ON SUBMIT CHECK IF ALL THE INPUTS ARE VALID
+
 import fbLogo from "../assets/facebook.png";
 import googleLogo from "../assets/google.png";
 import arrowPic from "../assets/arrow_signIn.png";
@@ -11,24 +13,104 @@ import {
   faUser,
   faEye,
   faCheck,
+  faEyeSlash,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import React, { Component } from "react";
 
-//<script src="https://kit.fontawesome.com/728d58002e.js" crossorigin="anonymous"></script>
-
-const passwordReg =
-  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]|.*?[#?!@$%^&*-]).{8,}$/gm;
-
-const handleAnimation = (event) => {
-  event.currentTarget.style.color = "salmon";
-};
-
+const numSymbolReg = /([0-9]|[#?!@$%^/&*-])/;
+const uppercaseReg = /([A-Z])/;
+const userNameReg =
+  /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/;
+const mailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 class Register extends Component {
   state = {
-    /*state for icons highlight */ user: false,
+    /*state for icons highlight */
+    user: false,
+    userValue: "",
+    userValueBool: false,
+    userAccepted: false,
     email: false,
+    emailValue: "",
+    emailValueBool: false,
+    emailAccepted: false,
     password: false,
+    passwordString: "",
+    passwordConditions: false,
+    lengthCondition: false,
+    numSymbolCondition: false,
+    uppercaseCondition: false,
     retype: false,
+    retypeValueBool: false,
+    eye: false,
+  };
+
+  ChangePasswordToText = () => {
+    /*switch beetween icons + password or text type*/
+    this.setState({
+      eye: !this.state.eye,
+    });
+  };
+
+  passwordChange = (event) => {
+    this.setState({ passwordString: event.target.value });
+
+    if (event.target.value.length > 0)
+      //show conditions condition
+      this.setState({ passwordConditions: true });
+    else this.setState({ passwordConditions: false });
+
+    if (event.target.value.length >= 8 && !event.target.value.includes(" "))
+      //the 8 caracters condition
+      this.setState({ lengthCondition: true });
+    else this.setState({ lengthCondition: false });
+
+    if (numSymbolReg.test(event.target.value)) {
+      //the 8 caracters condition
+      this.setState({ numSymbolCondition: true });
+      console.log(event.target.value, numSymbolReg.test(event.target.value));
+    } else this.setState({ numSymbolCondition: false });
+
+    if (uppercaseReg.test(event.target.value))
+      //the 8 caracters condition
+      this.setState({ uppercaseCondition: true });
+    else this.setState({ uppercaseCondition: false });
+  };
+
+  retypeChange = (event) => {
+    console.log(this.state.passwordString);
+    if (event.target.value == this.state.passwordString)
+      this.setState({ retypeValueBool: true });
+    else this.setState({ retypeValueBool: false });
+  };
+  userChange = (event) => {
+    this.setState({ userValue: event.target.value }); //set username value in the state
+
+    if (event.target.value == 0)
+      this.setState({
+        userValueBool: false,
+      });
+    //set userValueBool to false or true to decide if we show thie right icon or not
+    else this.setState({ userValueBool: true });
+
+    if (userNameReg.test(event.target.value)) {
+      this.setState({ userAccepted: true });
+    }
+    //check if the username matches with the regex if yes itshows the green color if no it shows the gray one
+    else this.setState({ userAccepted: false });
+  };
+  mailChange = (event) => {
+    //same as userChange
+    this.setState({ emailValue: event.target.value });
+    if (event.target.value == 0) this.setState({ emailValueBool: false });
+    else this.setState({ emailValueBool: true });
+
+    if (mailReg.test(event.target.value)) {
+      this.setState({ emailAccepted: true });
+      console.log(event.target.value);
+    }
+    //check if the username matches with the regex if yes itshows the green color if no it shows the gray one
+    else this.setState({ emailAccepted: false });
   };
 
   render() {
@@ -47,26 +129,32 @@ class Register extends Component {
             <input
               id="username"
               placeholder="Username"
+              autoComplete="off"
               onFocus={() =>
                 this.setState({ user: true })
               } /*user icon highlight added*/
               onBlur={() =>
                 this.setState({ user: false })
               } /*user icon highlight removed */
-            ></input>
+              value={this.state.userValue}
+              onChange={this.userChange}
+            />
             <label htmlFor="username" className="labelIcon">
               <FontAwesomeIcon
-                className="icona"
                 icon={faUser}
                 fontSize={30}
-                onClick={handleAnimation}
+                color={this.state.user ? "#2c3e50" : "#C3C3C3"}
               />
             </label>
             <label htmlFor="username" className="RightIcon">
-              {this.state.user ? (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="green" />
+              {this.state.userValueBool ? (
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  fontSize={30}
+                  color={this.state.userAccepted ? "green" : "#C3C3C3"}
+                />
               ) : (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="#C3C3C3" />
+                ""
               )}
             </label>
           </div>
@@ -77,21 +165,29 @@ class Register extends Component {
             <input
               id="email"
               placeholder="E-mail"
+              autoComplete="off"
               onFocus={() => this.setState({ email: true })}
               onBlur={() => this.setState({ email: false })}
-            ></input>
+              value={this.state.emailValue}
+              onChange={this.mailChange}
+            />
+
             <label htmlFor="email" className="labelIcon">
-              {this.state.email ? (
-                <FontAwesomeIcon icon={faAt} fontSize={30} color="#2c3e50" />
-              ) : (
-                <FontAwesomeIcon icon={faAt} fontSize={30} color="#C3C3C3" />
-              )}
+              <FontAwesomeIcon
+                icon={faAt}
+                fontSize={30}
+                color={this.state.email ? "#2c3e50" : "#C3C3C3"}
+              />
             </label>
-            <label htmlFor="username" className="RightIcon">
-              {this.state.email ? (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="green" />
+            <label htmlFor="email" className="RightIcon">
+              {this.state.emailValueBool ? (
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  fontSize={30}
+                  color={this.state.emailAccepted ? "green" : "#C3C3C3"}
+                />
               ) : (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="#C3C3C3" />
+                ""
               )}
             </label>
           </div>
@@ -100,41 +196,64 @@ class Register extends Component {
 
           <div className="formGroup">
             <input
-              type="password"
+              type={this.state.eye ? "text" : "password"}
               placeholder="Password"
               onFocus={() => this.setState({ password: true })}
               onBlur={() => this.setState({ password: false })}
+              onChange={this.passwordChange}
             ></input>
             <label htmlFor="password" className="labelIcon">
-              {this.state.password ? (
-                <FontAwesomeIcon
-                  icon={faUnlock}
-                  fontSize={30}
-                  color="#2c3e50"
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faUnlock}
-                  fontSize={30}
-                  color="#C3C3C3"
-                />
-              )}
+              <FontAwesomeIcon
+                icon={faUnlock}
+                fontSize={30}
+                color={this.state.password ? "#2c3e50" : "#C3C3C3"}
+              />
             </label>
-            <label htmlFor="username" className="RightIcon">
-              {this.state.password ? (
-                <FontAwesomeIcon icon={faEye} fontSize={30} color="green" />
-              ) : (
-                <FontAwesomeIcon icon={faEye} fontSize={30} color="#C3C3C3" />
-              )}
+            <label htmlFor="password" className="RightIcon eye">
+              <FontAwesomeIcon
+                icon={this.state.eye ? faEye : faEyeSlash}
+                fontSize={30}
+                color={this.state.eye ? "#2c3e50" : "#C3C3C3"}
+                onClick={this.ChangePasswordToText}
+              />
             </label>
           </div>
 
+          {this.state.passwordConditions ? (
+            <div>
+              <h4
+                style={{ color: this.state.lengthCondition ? "green" : "red" }}
+              >
+                <FontAwesomeIcon
+                  icon={this.state.lengthCondition ? faCheck : faXmark}
+                />
+                at least 8 characters
+              </h4>
+              <h4
+                style={{
+                  color: this.state.numSymbolCondition ? "green" : "red",
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={this.state.numSymbolCondition ? faCheck : faXmark}
+                />
+                at least one number or symbol
+              </h4>
+              <h4
+                style={{
+                  color: this.state.uppercaseCondition ? "green" : "red",
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={this.state.uppercaseCondition ? faCheck : faXmark}
+                />
+                at least one upper case
+              </h4>
+            </div>
+          ) : (
+            ""
+          )}
           <br />
-          <li>
-            <h4>at least 8 characters</h4>
-            <h4>at least one number or symbol</h4>
-            <h4>at least one upper case</h4>
-          </li>
 
           <br />
 
@@ -144,34 +263,31 @@ class Register extends Component {
               placeholder="Retype your password"
               onFocus={() => this.setState({ retype: true })}
               onBlur={() => this.setState({ retype: false })}
+              onChange={this.retypeChange}
             ></input>
             <label htmlFor="password" className="labelIcon">
-              {this.state.retype ? (
-                <FontAwesomeIcon
-                  icon={faUnlock}
-                  fontSize={30}
-                  color="#2c3e50"
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faUnlock}
-                  fontSize={30}
-                  color="#C3C3C3"
-                />
-              )}
+              <FontAwesomeIcon
+                icon={faUnlock}
+                fontSize={30}
+                color={this.state.retype ? "#2c3e50" : "#C3C3C3"}
+              />
             </label>
-            <label htmlFor="username" className="RightIcon">
-              {this.state.retype ? (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="green" />
+            <label htmlFor="password" className="RightIcon">
+              {this.state.retypeValueBool ? (
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  fontSize={30}
+                  color={this.state.retype ? "green" : "#C3C3C3"}
+                />
               ) : (
-                <FontAwesomeIcon icon={faCheck} fontSize={30} color="#C3C3C3" />
+                ""
               )}
             </label>
           </div>
 
           <div className="signUp">
             <p>
-              Already a member ? <a href="">Sign in</a>
+              Already a member ? <a href="/Login">Sign in</a>
             </p>
           </div>
 
