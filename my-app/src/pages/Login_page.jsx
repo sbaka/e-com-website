e900796ebe32608be17ef68df68e3f9c54b658d7
Maaaadox <ahmedmimen9@gmail.com>
2022-07-23 10:57:26 +0100

    done some stuff with register

--- my-app/src/pages/Login_page.jsx
+++ my-app/src/pages/Login_page.jsx
@@ -1,5 +1,6 @@
 import fbLogo from "../assets/facebook.png";
 import googleLogo from "../assets/google.png";
+<<<<<<< HEAD
 import arrowPic from "../assets/arrow_signIn.png";
 import "../css/Login_page.css";
 import "https://kit.fontawesome.com/728d58002e.js";
@@ -10,10 +11,20 @@ import {
   faArrowLeft,
   faEye,
 } from "@fortawesome/free-solid-svg-icons";
+=======
+import arrowPic from "../assets/arrow_signIn.png"
+import React from 'react';
+import "../css/Login_page.css"
+import "https://kit.fontawesome.com/728d58002e.js"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt, faUnlock, faArrowLeft, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
-//<script src="https://kit.fontawesome.com/728d58002e.js" crossorigin="anonymous"></script>
+
+>>>>>>> 979557111ca01dc3f90808fcf148c2e8f9a7a900
+
 
 function Login() {
+<<<<<<< HEAD
   return (
     <div className="parent">
       <div className="container">
@@ -64,5 +75,64 @@ function Login() {
       </div>
     </div>
   );
+=======
+    const [eye, setEye] = React.useState(); {/*to handle the changes on click of the icon*/ }
+    function ChangePasswordToText() {
+        {/*switch beetween icons + password or text type*/ }
+        if (eye) { setEye(false) }
+        if (!eye) { setEye(true) }
+    }
+
+
+    return (
+        <div className="parent">
+            <div className="container">
+                <button className="return">
+                    <FontAwesomeIcon icon={faArrowLeft} fontSize={30} />
+                </button>
+                <h2>Sign in</h2>
+                <h4>Fill in the form bellow to continue</h4>
+
+                <div className="formGroup">
+                    <input id="email" placeholder="E-mail"></input>
+                    <label htmlFor="email" className="labelIcon">
+                        <FontAwesomeIcon icon={faAt} fontSize={30} color="#C3C3C3" />
+                    </label>
+                </div>
+
+
+                <br />
+
+                <div className="formGroup">
+                    <input type={eye ? "text" : "password"} placeholder="Password"></input>
+                    <label htmlFor="password" className="labelIcon">
+                        <FontAwesomeIcon icon={faUnlock} fontSize={30} color="#C3C3C3" />
+                    </label>
+                    <label className="eye">
+                        <FontAwesomeIcon
+                            icon={eye ? faEye : faEyeSlash}
+                            fontSize={30} color="#C3C3C3"
+                            className="eyeicon"
+                            onClick={ChangePasswordToText} />
+                    </label>
+                </div>
+
+                <div className="signUp">
+                    <p>You don't have an account yet ? <a href="">Sign up</a></p>
+                </div>
+
+                <div className="bottomDiv">
+                    <button type="submit" className="submitBtn"><p>Sign in </p><img id="arrow" src={arrowPic} alt="" /></button>
+                    Or
+                    <div>
+                        <img src={fbLogo} alt="fb" />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src={googleLogo} alt="google" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+>>>>>>> 979557111ca01dc3f90808fcf148c2e8f9a7a900
 }
 export default Login;
