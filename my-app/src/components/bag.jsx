67131a035bf9f0adad6c690e00b6b71b069dcd67
BeeLTL <belkacemtouil99@gmail.com>
2022-07-27 18:44:54 +0100

    add quantity backend + lil css modification

--- my-app/src/components/bag.jsx
+++ my-app/src/components/bag.jsx
@@ -4,52 +4,75 @@ import {
   faCircleArrowRight,
   faCircleArrowLeft,
   faCircleXmark
-  } from "@fortawesome/free-solid-svg-icons";
-  
- import styles from "../css/Bag.module.css";
+} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "../css/Bag.module.css"
+
+/* NOT FINAL NEED SOME IMPROVMENT IN CSS BUT GOOD FOR A BEGINING */
+/* 
+1- FIX ZOOM PROBLEMS 
+2- COUNTER PROBLEMS
+3- CSS ADJUSTMENT
+*/
+
 
 class Bag extends Component {
-    state = {  } 
-    /* NOT FINAL NEED SOME IMPROVMENT IN CSS BUT GOOD FOR A BEGINING */
-     /* 
-     1- FIX ZOOM PROBLEMS 
-     2- COUNTER PROBLEMS
-     3- CSS ADJUSTMENT
-     */
-   
-    render() { 
-        return (
-       <div className={styles.bagcontainer} >
+  state = {
+    quantity: 1,
+  }
+
+  addquantity = () => {
+    this.setState({
+      quantity: this.state.quantity + 1
+    });
+  }
+  subtractquantity = () => {
+    if (this.state.quantity == 1) {
+      this.setState({
+        quantity: this.state.quantity = 1
+      });
+    }
+    else this.setState({
+      quantity: this.state.quantity + -1
+    });
+  }
+    
+ 
+
+
+  render() {
+    return (
+      <div className={styles.bagcontainer} >
         <div className={styles.bag}>
-         <div className={styles.productcard}>
-                <img className={styles.productcardimg} src="https://cdn.shopify.com/s/files/1/0605/1338/6692/products/E22CHECL0029_BLF3_hd-mface_533x.jpg?v=1646930100">
-                </img>
-            <div >
-              <p className={styles.productcarddescription}> chemise</p>
+          <div className={styles.productcard}>
+            <div className={styles.leftsidebag } >
+            <img className={styles.productcardimg} src="https://cdn.shopify.com/s/files/1/0605/1338/6692/products/E22CHECL0029_BLF3_hd-mface_533x.jpg?v=1646930100">
+            </img>
+            <p className={styles.productcarddescription}> chemise</p>
             </div>
-           <div >
-           <p className={styles.productcardprice}>2500DZD</p>
-           </div>
-            <div className={styles.itemcount}>
-              <FontAwesomeIcon icon={faCircleArrowLeft} fontSize={30} />
-              <input type="number" className={styles.productcount} defaultValue="1" min="1"></input>  
-              <FontAwesomeIcon icon={faCircleArrowRight} fontSize={30} />
+            <div >
+              <p className={styles.productcardprice}>2500DZD</p>
+            </div>
+            <div className={styles.quantity}>
+              <FontAwesomeIcon icon={faCircleArrowLeft} cursor={"pointer"} fontSize={30} onClick={this.subtractquantity} />
+              <input type="number" className={styles.productcount} defaultValue="1" value={this.state.quantity} ></input>
+              <FontAwesomeIcon icon={faCircleArrowRight} cursor={"pointer"} fontSize={30} onClick={this.addquantity} />
             </div>
             <div>
-            <p class={styles.productcardrealprice}>5000DZD</p>
+              <p class={styles.productcardrealprice}>5000DZD</p>
             </div>
-            <div> 
-            <FontAwesomeIcon icon={faCircleXmark} className={styles.productdeletebutton} fontSize={30} />
+            <div>
+              <FontAwesomeIcon icon={faCircleXmark} className={styles.productdeletebutton} fontSize={30} />
             </div>
-        
 
-         </div>
 
-         </div>
-         </div>
-         
-        );
-    }
+          </div>
+
+        </div>
+      </div>
+
+    );
+  }
 }
- 
-export default Bag ;
+
+export default Bag;
