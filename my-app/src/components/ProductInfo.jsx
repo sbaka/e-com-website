2ce592d6c1903e196a3031a2a22f7de572f63ba0
unknown <191937050983@etu.univ-usto.dz>
2022-07-28 12:35:34 +0100

    almost done with product page/need more css work

--- my-app/src/components/ProductInfo.jsx
+++ my-app/src/components/ProductInfo.jsx
@@ -6,7 +6,7 @@ class ProductInfo extends Component{
     render(){
         return(
         <div className={style.container}>    
-            <div className={style.info}>
+            <div className={style.info}>{/*product description*/} 
                 <div>
                     <p>Homme / Chemises</p>
                     <h3>{productData.chemises[0].description}</h3>
@@ -27,14 +27,54 @@ class ProductInfo extends Component{
                     lorem ipsum lorem ipsum lorem ipsum </p>
                 </div>
             </div>
-            <div className={style.pics}>
+            <div className={style.pics}>{/*product pictures*/} 
                 <img className={style.mainpic} src={productData.chemises[0].img} />
                 <img className={style.secondarypics} src={productData.chemises[0].img} />
                 <img className={style.secondarypics} src={productData.chemises[0].img} />
                 <img className={style.secondarypics} src={productData.chemises[0].img} />
             </div>
-            <div>
-                
+            <div className={style.availability}>{/*what sizes and colors are available*/} 
+                <div>
+                    <h3>SIZES AVAILABLE</h3>
+                    <ul>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>XS</p>
+                        </li>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>S</p>
+                        </li>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>M</p>
+                        </li>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>L</p>
+                        </li>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>XL</p>
+                        </li>
+                        <li className={style.boxes}>
+                            <p className={style.sizes}>XXL</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <hr className={style.hr}/>
+                    <h3>COLORS AVAILABLE</h3>
+                    <ul>
+                        <li className={style.boxes} style={{backgroundColor: "red"}}>
+                        </li>
+                        <li className={style.boxes} style={{backgroundColor: "blue"}}>
+                        </li>
+                        <li className={style.boxes} style={{backgroundColor: "black"}}>
+                        </li>
+                        <li className={style.boxes} style={{backgroundColor: "green"}}>
+                        </li>
+                    </ul>
+                </div>
+                <div className={style.purchase}>
+                    <hr className={style.hr}/>
+                    <button>ADD TO CART</button>
+                </div>
             </div>
         </div>
         )
