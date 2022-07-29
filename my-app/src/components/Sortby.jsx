dd9bb9ea458be9c4e63cb6b8601b2a1bc4323fdf
Touil  Belkacem <89422210+BeeLTL@users.noreply.github.com>
2022-07-29 11:53:46 +0100

    better select

--- my-app/src/components/Sortby.jsx
+++ my-app/src/components/Sortby.jsx
@@ -1,26 +1,23 @@
 import React, { Component } from "react";
-
+import Select from 'react-select'
 import styles from "../css/Sortby.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+/* Dummy dat just to see the combobox*/
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
 class SortBy extends Component {
+  
   state = {};
   render() {
     return (
       <React.Fragment>
-        <select name="pets" id="pet-select" className={styles.combobox}>
-          {" "}
-          {/*raha sans arrow*/}
-          <option hidden value="">
-            SortBy
-          </option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
-          <option value="parrot">Parrot</option>
-          <option value="spider">Spider</option>
-          <option value="goldfish">Goldfish</option>
-        </select>
+     <Select options={options} placeholder="Sortby"/>
       </React.Fragment>
     );
   }
