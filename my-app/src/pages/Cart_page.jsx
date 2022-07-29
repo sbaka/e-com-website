9db9529a979c242a54c665466b1b6c1aaa4e3288
Latif_Kbr <abdelatif4614@gmail.com>
2022-07-29 14:06:35 +0100

    fixing bugs still

--- my-app/src/pages/Cart_page.jsx
+++ my-app/src/pages/Cart_page.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 import OrderInfo from "../components/OrderInfo";
+import CartItem from '../components/CartItem'
 import styles from "../css/Cart.module.css"
 class Cart extends Component {
-    state = {}
-    render() {
-        return (
-            <React.Fragment>
-                <Header />
-                <div className={styles.mainContainer}>
-                    <CartItem/>
-                    <div className={styles.order}>
-                        <OrderInfo/>
-                    </div>
-                    {/* i use this for spacing */}
-                    <div className={styles.empty}>
-                        
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div className={styles.mainContainer}>
+          <CartItem />
+          <div className={styles.order}>
+            <OrderInfo />
+          </div>
+          {/* i use this for spacing */}
+          <div className={styles.empty}>
+
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Cart;
