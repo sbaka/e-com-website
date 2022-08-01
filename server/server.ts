1a6db7472fac31f3f71d97665f6ce441b5f31788
sbaka <abdelatif4614@gmail.com>
2022-08-01 13:57:22 +0100

    cors

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes.ts";
 const port = 6969;
 
@@ -9,7 +9,8 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(
   oakCors({
-    origin: "*",
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
 
