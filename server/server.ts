a23601d1fc833c57063bd3ccf6f4debabb32078e
sbaka <abdelatif4614@gmail.com>
2022-08-01 13:31:53 +0100

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ app.use(
   oakCors({
     origin: "http://localhost:3000",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    preflightContinue: false,
+    preflightContinue: true,
   })
 );
 
