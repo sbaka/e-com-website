41fc4ad0dbacab0a6c85006de6188327f62e129c
sbaka <abdelatif4614@gmail.com>
2022-08-01 13:54:31 +0100

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import router from "./routes.ts";
 const port = 6969;
 
